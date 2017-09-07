ae404831b60bb04c4801be5b88d99b010ba5e636
yuanjunjie <yuanjunjie@kedacom.com>
2017-09-07 16:26:26 +0800

    .

--- cpp/15. 3Sum.cpp
+++ cpp/15. 3Sum.cpp
@@ -13,7 +13,48 @@ A solution set is:
 ]
 */
 
-class Solution {
+class Solution
+{
 public:
-  vector<vector<int>> threeSum(vector<int> &nums) {}
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        std::sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.size() - 1;
+            int target = -nums[i];
+            while (j < k)
+            {
+                if (nums[j] + nums[k] == target)
+                {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    ++j;
+                    --k;
+                    while (j < k && nums[j] == nums[j - 1])
+                    {
+                        ++j;
+                    }
+                    while (j < k && nums[k] == nums[k - 1])
+                    {
+                        --j;
+                    }
+                }
+                else if (nums[j] + nums[k] < target)
+                {
+                    ++j;
+                }
+                else
+                {
+                    --k;
+                }
+            }
+        }
+        return res;
+    }
 };
