a37da89a5b40c69bfc87bc4d9c1409d20b309144
yjjnls <x-jj@foxmail.com>
2017-09-11 23:30:07 +0800

    .

--- cpp/80. Remove Duplicates from Sorted Array II.cpp
+++ cpp/80. Remove Duplicates from Sorted Array II.cpp
@@ -10,7 +10,35 @@ being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
 
 */
 
-class Solution {
+class Solution
+{
 public:
-  int removeDuplicates(vector<int> &nums) {}
+    int removeDuplicates(vector<int> &nums)
+    {
+        if (nums.size() <= 2)
+        {
+            return nums.size();
+        }
+        int i = 2;
+        for (int j = i + 1; j < nums.size(); ++j)
+        {
+            if (nums[j] > nums[i - 2])
+            {
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
 };
+
+//time:O(N)
+//space:O(1)
+
+int removeDuplicates(vector<int> &nums)
+{
+    int i = 0;
+    for (int n : nums)
+        if (i < 2 || n > nums[i - 2])
+            nums[i++] = n;
+    return i;
+}
