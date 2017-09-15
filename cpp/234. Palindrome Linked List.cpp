4b8e53cfe201e35ded3dfc7eae5e8295307244c7
yuanjunjie <yuanjunjie@kedacom.com>
2017-09-15 16:17:28 +0800

    .

--- cpp/234. Palindrome Linked List.cpp
+++ cpp/234. Palindrome Linked List.cpp
@@ -18,5 +18,61 @@ class Solution
 public:
     bool isPalindrome(ListNode *head)
     {
+        if (head == NULL || head->next == NULL)
+            return true;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        slow = reverseList(slow->next);
+        while (slow != NULL)
+        {
+            if (head->val != slow->val)
+                return false;
+            head = head->next;
+            slow = slow->next;
+        }
+        return true;
+    }
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *pre = NULL;
+        ListNode *next = NULL;
+        while (head != NULL)
+        {
+            next = head->next;
+            head->next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
     }
 };
+
+/*
+1.reach the middle of the list
+there is n elements in the list, when the fast pointer stops
+
+slow = (n+1)/2      n is odd
+    1->2->3->2->1->NULL
+          👆       👆
+          slow     fast
+slow = n/2          n is even
+    1->2->2->1->NULL
+       👆 👆
+       slow fast
+
+2. reverse the rest half list from slow->next, slow points to 
+    1->2->3->NULL
+    👆 
+    slow 
+
+    1->2->NULL
+    👆
+    slow
+
+3. compare slow and head, util slow is null
+*/
