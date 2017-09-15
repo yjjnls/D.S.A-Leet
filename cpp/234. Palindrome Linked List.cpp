989c79c3d985ef28d50679f4b24c862ab3efbf7a
yuanjunjie <yuanjunjie@kedacom.com>
2017-09-15 14:41:40 +0800

    .

---
+++ cpp/234. Palindrome Linked List.cpp
@@ -0,0 +1,22 @@
+/*
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isPalindrome(ListNode *head)
+    {
+    }
+};
