e3c5ce72360af22ace6dc1648bc3409d9e332269
yjjnls <x-jj@foxmail.com>
2017-09-23 22:48:12 +0800

    .

--- cpp/98. Validate Binary Search Tree.cpp
+++ cpp/98. Validate Binary Search Tree.cpp
@@ -29,9 +29,43 @@ Binary tree [1,2,3], return false.
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- class Solution {
+class Solution
+{
 public:
-    bool isValidBST(TreeNode* root) {
-        
+    bool isValidBST(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return true;
+        }
+        if (!isValidBST(root->left))
+        {
+            return false;
+        }
+        //pre_ is the max val of the left subtree
+        if (root->val <= pre_)
+        {
+            return false;
+        }
+        else
+        {
+            pre_ = root->val;
+        }
+
+        if (!isValidBST(root->right))
+        {
+            return false;
+        }
+        return true;
     }
+
+private:
+    //c++11 permission
+    /*use LONG_MIN instead of INT_MIN, if the input is just INT_MIN, it will be wrong.
+    */
+    long pre_ = LONG_MIN;
 };
+//time:O(N)
+//1. inorder -> is increased array
+
+//2.getMax(root->left)<=root  && root <= getMin(root->right)
