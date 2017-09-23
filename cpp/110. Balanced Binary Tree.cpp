793f7eac5ea3e0f056abd0406af3444619026219
yjjnls <x-jj@foxmail.com>
2017-09-24 00:10:26 +0800

    .

--- cpp/110. Balanced Binary Tree.cpp
+++ cpp/110. Balanced Binary Tree.cpp
@@ -47,4 +47,32 @@ public:
     }
 };
 
-//dp
+//dp time:O(N^2) top to down method
+
+//solution2 DFS
+class solution
+{
+public:
+    int dfsHeight(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+
+        int leftHeight = dfsHeight(root->left);
+        if (leftHeight == -1)
+            return -1;
+        int rightHeight = dfsHeight(root->right);
+        if (rightHeight == -1)
+            return -1;
+
+        if (abs(leftHeight - rightHeight) > 1)
+            return -1;
+        return max(leftHeight, rightHeight) + 1;
+    }
+    bool isBalanced(TreeNode *root)
+    {
+        return dfsHeight(root) != -1;
+    }
+};
+
+//time:O(N)
