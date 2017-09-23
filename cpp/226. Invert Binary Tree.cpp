c290afe95cf9c9392030af523611a829c7386fe0
yjjnls <x-jj@foxmail.com>
2017-09-23 21:47:06 +0800

    .

--- cpp/226. Invert Binary Tree.cpp
+++ cpp/226. Invert Binary Tree.cpp
@@ -26,9 +26,49 @@ Google: 90% of our engineers use the software you wrote (Homebrew), but you canâ
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- class Solution {
+class Solution
+{
 public:
-    TreeNode* invertTree(TreeNode* root) {
-        
+    TreeNode *invertTree(TreeNode *root)
+    {
+        //recursion terminator
+        //current level processing
+        //drill down
+
+        if (root == NULL)
+        {
+            return NULL;
+        }
+
+        std::swap(root->left, root->right);
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
+
+//solution 2 no-recursion
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        std::stack<TreeNode *> stk;
+        stk.push(root);
+
+        while (!stk.empty())
+        {
+            TreeNode *p = stk.top();
+            stk.pop();
+            if (p)
+            {
+                stk.push(p->left);
+                stk.push(p->right);
+                std::swap(p->left, p->right);
+            }
+        }
+        return root;
     }
 };
