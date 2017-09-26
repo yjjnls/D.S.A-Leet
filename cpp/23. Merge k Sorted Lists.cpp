f9b3b2a5f976cfb8bddb907d2438a9c858107071
yuanjunjie <yuanjunjie@kedacom.com>
2017-09-26 16:37:53 +0800

    important

--- cpp/23. Merge k Sorted Lists.cpp
+++ cpp/23. Merge k Sorted Lists.cpp
@@ -10,9 +10,59 @@ Merge k sorted linked lists and return it as one sorted list. Analyze and descri
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- class Solution {
+class Solution
+{
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
+    struct compare
+    {
+        bool operator()(const ListNode *l, const ListNode *r)
+        {
+            if (l == NULL || r == NULL)
+            {
+                return false;
+            }
+            return l->val > r->val;
+        }
+    };
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        if (lists.empty())
+        {
+            return NULL;
+        }
+        priority_queue<ListNode *, vector<ListNode *>, compare> q;
+        for (auto it : lists)
+        {
+            //这一句很重要
+            if (it != NULL)
+            {
+                q.push(it);
+            }
+        }
+        ListNode dummy(0);
+        ListNode *result = &dummy;
+        while (!q.empty())
+        {
+            if (result != NULL)
+                result->next = q.top();
+            result = result->next;
+            q.pop();
+            if (result != NULL && result->next != NULL)
+            {
+                q.push(result->next);
+            }
+        }
+        return dummy.next;
     }
 };
+
+/*
+solution 1
+merge the list one by one, each process is a `merge 2 sorted lists` problem.
+time:O(N^2)
+*/
+
+/*
+solution 2
+use priority_queue
+*/
