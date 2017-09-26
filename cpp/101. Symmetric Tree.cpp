2e496d0e9ed422e01436f2ebed0fbb9e65ae1181
yuanjunjie <yuanjunjie@kedacom.com>
2017-09-26 13:39:48 +0800

    .

--- cpp/101. Symmetric Tree.cpp
+++ cpp/101. Symmetric Tree.cpp
@@ -32,5 +32,74 @@ class Solution
 public:
     bool isSymmetric(TreeNode *root)
     {
+        inorder(root);
+        for (int i = 0, j = nums_.size() - 1; i < j; ++i, --j)
+        {
+            if (nums_[i] != nums_[j])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    void inorder(TreeNode *root)
+    {
+        if (root != NULL)
+        {
+            inorder(root->left);
+            nums_.push_back(root->val);
+            inorder(root->right);
+        }
+    }
+
+private:
+    vector<int> nums_;
+};
+
+//solution 1
+//inorder traverse and check if it's a palindrome string.
+//time:O(2*N)
+//space:O(N)
+/*★ the method is wrong, if the tree is like this:
+            1
+        2       3
+    3       2
+
+*/
+
+//solution 2
+//recursion like a mirror
+class Solution
+{
+public:
+    bool isSymmetric(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return true;
+        }
+        return isMirror(root->left, root->right);
+    }
+    bool isMirror(TreeNode *p1, TreeNode *p2)
+    {
+        //1.recursion termiantor
+        if (p1 == NULL && p2 == NULL)
+        {
+            return true;
+        }
+        else if (p1 == NULL || p2 == NULL)
+        {
+            return false;
+        }
+        //2.current level processing
+        if (p1->val != p2->val)
+        {
+            return false;
+        }
+
+        //drill down
+        return isMirror(p1->left, p2->right) && isMirror(p1->right, p2->left);
     }
 };
+//time:O(N)
+//space:O(1)
