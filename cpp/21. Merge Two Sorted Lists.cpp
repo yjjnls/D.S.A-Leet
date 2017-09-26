898e8fbf0b9b8ae053f39881e42a335435753b42
yuanjunjie <yuanjunjie@kedacom.com>
2017-09-26 14:33:16 +0800

    .

--- cpp/21. Merge Two Sorted Lists.cpp
+++ cpp/21. Merge Two Sorted Lists.cpp
@@ -17,7 +17,23 @@ public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
     {
         ListNode dummy(INT_MIN);
-        ListNode 
+        ListNode *node = &dummy;
+        while (l1 != NULL && l2 != NULL)
+        {
+            if (l1->val < l2->val)
+            {
+                node->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                node->next = l2;
+                l2 = l2->next;
+            }
+            node = node->next;
+        }
+        node->next = l1 == NULL ? l2 : l1;
+        return dummy.next;
     }
 };
 //time:O(N)
