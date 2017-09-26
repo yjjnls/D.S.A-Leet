4159d4de8ee95b890a4dc0ae7facf39f5e06b61a
yuanjunjie <yuanjunjie@kedacom.com>
2017-09-26 16:37:32 +0800

    .

--- cpp/54. Spiral Matrix.cpp
+++ cpp/54. Spiral Matrix.cpp
@@ -20,3 +20,29 @@ public:
         
     }
 };
+
+//discussion里面的解法，这题目有什么意思？
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty()) return {};
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> spiral(m * n);
+        int u = 0, d = m - 1, l = 0, r = n - 1, k = 0;
+        while (true) {
+            // up
+            for (int col = l; col <= r; col++) spiral[k++] = matrix[u][col];
+            if (++u > d) break;
+            // right
+            for (int row = u; row <= d; row++) spiral[k++] = matrix[row][r];
+            if (--r < l) break;
+            // down
+            for (int col = r; col >= l; col--) spiral[k++] = matrix[d][col];
+            if (--d < u) break;
+            // left
+            for (int row = d; row >= u; row--) spiral[k++] = matrix[row][l];
+            if (++l > r) break;
+        }
+        return spiral;
+    }
+};
