7eb546c6fb663d79dcda1052a976e81aa8445edc
yjjnls <x-jj@foxmail.com>
2017-10-15 12:32:42 +0800

    .

--- doc/graph.md
+++ doc/graph.md
@@ -4,13 +4,21 @@
 需要用hash表或者stack来保存已经访问过的结点
 [演示过程](http://www.cs.usfca.edu/~galles/visualization/DFS.html)
 
+<!-- ![dfs](./img/Graph/dfs.png) -->
+![dfs_gif](./img/Graph/dfs.gif)
 ### 模版
 DFS可以用递归，如果不用递归，则用stack来维护
 
 时间复杂度为O(N)，每个结点只访问一次
 
+递归做法
+* 200 Number of Islands
+* 79 Word Search
+
 ## BFS
 [display](http://www.cs.usfca.edu/~galles/visualization/BFS.html)
+<!-- ![dfs](./img/Graph/bfs.png) -->
+![dfs_gif](./img/Graph/bfs.gif)
 
 BFS不能用递归，因为递归是往下深入一层，与BFS的语义就违背了。所以BFS只能用队列来维护。
 time:O(N)
--- src/cpp/79. Word Search.cpp
+++ src/cpp/79. Word Search.cpp
@@ -23,5 +23,48 @@ class Solution
 public:
     bool exist(vector<vector<char>> &board, string word)
     {
+        int row = board.size();
+        int col = -1;
+        for (int i = 0; i < row; ++i)
+        {
+            col = board[i].size();
+            for (int j = 0; j < col; j++)
+            {
+                if (found_dfs(board, i, j, word, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    bool found_dfs(vector<vector<char>> &board, int x, int y, string &word, int pos)
+    {
+        //recursion terminator
+        if (word[pos] == '\0')
+            return true;
+        if (x < 0 || y < 0 || x == board.size() || y == board[x].size())
+            return false;
+        if (board[x][y] != word[pos])
+            return false;
+
+        //current level processing
+        //因为ascii码从0-127，抑或上256，就把该字母变成非字母
+        //但是这里对c++不管用
+        char tmp = board[x][y];
+        board[x][y] = -1;
+
+        //drill down
+        bool res = found_dfs(board, x - 1, y, word, pos + 1) ||
+                   found_dfs(board, x + 1, y, word, pos + 1) ||
+                   found_dfs(board, x, y - 1, word, pos + 1) ||
+                   found_dfs(board, x, y + 1, word, pos + 1);
+
+        //reverse current level status
+        board[x][y] = tmp;
+
+        return res;
     }
 };
+
+//time:O(M*N*K)
+//k为单词平均长度
