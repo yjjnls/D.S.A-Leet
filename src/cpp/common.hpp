59ce578ab2c61921292a60113df8bd8f3c006d1d
yjjnls <x-jj@foxmail.com>
2017-10-17 00:31:02 +0800

    .

---
+++ src/cpp/common.hpp
@@ -0,0 +1,32 @@
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+#include <list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x)
+        : val(x), next(NULL)
+    {
+    }
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x)
+        : val(x), left(NULL), right(NULL)
+    {
+    }
+};
