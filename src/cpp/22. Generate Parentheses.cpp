e11ea8432b359033dde0ceeef3989c54c278b445
yjjnls <x-jj@foxmail.com>
2017-10-18 23:25:43 +0800

    .

--- src/cpp/22. Generate Parentheses.cpp
+++ src/cpp/22. Generate Parentheses.cpp
@@ -19,5 +19,24 @@ class Solution
 public:
     vector<string> generateParenthesis(int n)
     {
+        _dfs(string(""), n, 0);
+        return res;
     }
+    void _dfs(string s, int left, int right)
+    {
+        if (left == 0 && right == 0)
+        {
+            res.push_back(s);
+            return;
+        }
+        if (right > 0)
+            _dfs(s + ")", left, right - 1);
+        if (left > 0)
+            _dfs(s + "(", left - 1, right + 1);
+    }
+
+private:
+    vector<string> res;
 };
+
+//这和dfs有什么关系？
