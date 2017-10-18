a06cf5291107122a90f0ce454b20f51402526501
yjjnls <x-jj@foxmail.com>
2017-10-19 00:31:02 +0800

    .

---
+++ src/cpp/141. Linked List Cycle.cpp
@@ -0,0 +1,37 @@
+/*
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <common.hpp>
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+            return false;
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
--- src/cpp/142. Linked List Cycle II.cpp
+++ src/cpp/142. Linked List Cycle II.cpp
@@ -14,10 +14,49 @@ Can you solve it without using extra space?
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+#include <common.hpp>
 class Solution
 {
 public:
     ListNode *detectCycle(ListNode *head)
     {
+        if (head == NULL || head->next == NULL)
+            return NULL;
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *entry = head;
+
+        while (fast->next != NULL && fast->next->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+            {
+                while (fast != entry)
+                {
+                    fast = fast->next;
+                    entry = entry->next;
+                }
+                return entry;
+            }
+        }
+        return NULL;
     }
 };
+
+/*
+快慢指针在第k步相遇，圆环长度为r
+2k-k=nr
+k=nr
+
+第一个相遇点为s
+k-s=m
+s=nr-m=(n-1)r+r-m
+这里取n=1,则得到
+s=r-m
+s是从head到entry的距离
+m是s到k的距离，由于s和k都在环上，所以r-m极为k到s的距离，即fast指针再走到entry的距离
+
+感觉是纯数学问题！
+*/
