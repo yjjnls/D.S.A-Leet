862df83fc67faeac60b6fcfd12e0abf89f28f7f9
yjjnls <x-jj@foxmail.com>
2017-11-19 10:36:21 +0800

    .

--- src/cpp/164. Maximum Gap.cpp
+++ src/cpp/164. Maximum Gap.cpp
@@ -27,6 +27,7 @@ public:
         int max_num = *std::max_element(nums.begin(), nums.end());
 
         // n个元素，分为n-1个区间，n个桶
+        // ！！！最后一个数一定落在最后一个桶里面
         int bucketSize = std::max(1, (max_num - min_num) / ((int)nums.size() - 1));
         int bucketNum = (max_num - min_num) / bucketSize + 1;
         vector<Bucket> buckets(bucketNum);
@@ -40,6 +41,19 @@ public:
             buckets[bucketIdx].minval = std::min(num, buckets[bucketIdx].minval);
             buckets[bucketIdx].maxval = std::max(num, buckets[bucketIdx].maxval);
         }
+
+        int prev_max = min_num;
+        int gap = 0;
+        // 遍历非空桶之间的gap，求最大值
+        for (auto &bucket : buckets)
+        {
+            if (!bucket.used)
+                continue;
+
+            gap = std::max(gap, bucket.minval - prev_max);
+            prev_max = bucket.maxval;
+        }
+        return gap;
     }
 };
 
@@ -53,12 +67,15 @@ solution 2
 先遍历求出最大值与最小值的差距k，创建数组（k个元素）
 将所有数据插入到相应的位置，然后遍历数组，找到空隙最大的即可
 time:O(N)
+space:O(k) k>=N
 ！！！但是如果数据的间隙很大（1，2，10000）效率就不高
 这里可以用bit compressed库，但在算法题里面就不太合适
 
 solution 3
 桶排序
-
+构建n个桶，每个桶记录最大值和最小值，遍历非空桶之间的gap即可
+time：O(N)
+space：O(N)
 
 */
 class Solution
@@ -66,5 +83,41 @@ class Solution
 public:
     int maximumGap(vector<int> &nums)
     {
+        if (nums.empty() || nums.size() < 2)
+            return 0;
+
+        int maxVal = *max_element(nums.begin(), nums.end());
+
+        int exp = 1;   // 1, 10, 100, 1000 ...
+        int radix = 10;// base 10 system
+
+        vector<int> aux(nums.size());
+
+        /* LSD Radix Sort */
+        while (maxVal / exp > 0)
+        {// Go through all digits from LSD to MSD
+            vector<int> count(radix, 0);
+
+            for (int i = 0; i < nums.size(); i++)// Counting sort
+                count[(nums[i] / exp) % 10]++;
+
+            for (int i = 1; i < count.size(); i++)// you could also use partial_sum()
+                count[i] += count[i - 1];
+
+            for (int i = nums.size() - 1; i >= 0; i--)
+                aux[--count[(nums[i] / exp) % 10]] = nums[i];
+
+            for (int i = 0; i < nums.size(); i++)
+                nums[i] = aux[i];
+
+            exp *= 10;
+        }
+
+        int maxGap = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++)
+            maxGap = max(nums[i + 1] - nums[i], maxGap);
+
+        return maxGap;
     }
 };
