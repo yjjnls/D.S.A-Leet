8b65e23bd734ebadecbc9bdf6fa04824185cc81f
yuanjunjie <yuanjunjie@kedacom.com>
2017-11-21 18:26:10 +0800

    update test

--- test/cpp/gtest/build.sh
+++ test/cpp/gtest/build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 __dir__=$( cd $(dirname ${BASH_SOURCE[0]}); pwd )
 
+mkdir -p build && cd build
+rm -rf *
+
+mkdir -p $__dir__/build/include
+
+mkdir -p $__dir__/build/lib
+
+
+
 wget https://github.com/google/googletest/archive/release-1.8.0.tar.gz
 tar xf release-1.8.0.tar.gz
 cd googletest-release-1.8.0
 cmake -DBUILD_SHARED_LIBS=ON .
 
 make
-make -p __dir__/include
-make -p __dir__/lib
-cp -af googletest/include __dir__/include
-cp -af googlemock/gtest/libgtest_main.so googlemock/gtest/libgtest.so __dir__/lib
-
+cp -af googletest/include/gtest $__dir__/build/include
+cp -af googlemock/gtest/libgtest_main.so googlemock/gtest/libgtest.so $__dir__/build/lib
