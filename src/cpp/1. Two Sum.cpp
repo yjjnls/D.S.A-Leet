fa12a7b3782b7ffb46e5c1a6d18d2e9cb7fbfe4d
yjjnls <x-jj@foxmail.com>
2017-11-29 23:56:43 +0800

    update test

--- src/cpp/1. Two Sum.cpp
+++ src/cpp/1. Two Sum.cpp
@@ -44,7 +44,7 @@ If there are duplicated elements, cache will be overwritten when scanned
 the same element in different positions. [3,2,2,4]->cache[2]=1->cache[2]=2
 If output is the value, it's OK! while if it is index, it will be wrong;
 */
-
+#ifdef USE_GTEST
 TEST(DSA, 1_Two_Sum)
 {
     vector<int> nums = {2, 7, 11, 15};
@@ -54,3 +54,4 @@ TEST(DSA, 1_Two_Sum)
     for (int i = 0; i < 2; ++i)
         ASSERT_TRUE(res[i] == result[i]);
 }
+#endif
