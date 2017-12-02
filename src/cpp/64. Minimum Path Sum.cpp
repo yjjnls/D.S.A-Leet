aea0a741050d34160ef7786ae038e57f21b3f63a
yjjnls <x-jj@foxmail.com>
2017-12-02 23:12:33 +0800

    update test

--- src/cpp/64. Minimum Path Sum.cpp
+++ src/cpp/64. Minimum Path Sum.cpp
@@ -15,5 +15,34 @@ class Solution
 public:
     int minPathSum(vector<vector<int>> &grid)
     {
+        int m = grid.size();
+        int n = grid[0].size();
+        //处理边界
+        for (int i = 1; i < n; ++i)
+            grid[0][i] += grid[0][i - 1];
+
+        for (int i = 1; i < m; ++i)
+            grid[i][0] += grid[i - 1][0];
+
+        //递推
+        for (int i = 1; i < m; ++i)
+            for (int j = 1; j < n; ++j)
+                grid[i][j] += std::min(grid[i - 1][j], grid[i][j - 1]);
+
+        return grid[m - 1][n - 1];
     }
 };
+//DP
+//time:O(n)
+#ifdef USE_GTEST
+TEST(DSA, 64_minPathSum)
+{
+    vector<int> tmp1 = {1, 3, 1};
+    vector<int> tmp2 = {1, 5, 1};
+    vector<int> tmp3 = {4, 2, 1};
+    vector<vector<int>> grid = {tmp1, tmp2, tmp3};
+    Solution s;
+    int res = s.minPathSum(grid);
+    ASSERT_TRUE(res == 7);
+}
+#endif
