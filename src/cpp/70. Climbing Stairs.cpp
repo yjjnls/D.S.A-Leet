ee53c19c75a96d55d3dd3182b4d5dff621fc7e56
yjjnls <x-jj@foxmail.com>
2017-12-02 23:47:25 +0800

    add test

--- src/cpp/70. Climbing Stairs.cpp
+++ src/cpp/70. Climbing Stairs.cpp
@@ -11,5 +11,48 @@ class Solution
 public:
     int climbStairs(int n)
     {
+        if (n < 0)
+            return 0;
+        if (n == 0)
+            return 1;
+
+        return climbStairs(n - 1) + climbStairs(n - 2);
     }
 };
+
+/*
+上述solution是递归版的dp，可以说是从上往下
+方法是对的，但是n大了之后time limit exceeded
+
+迭代版的dp都是从下往上的
+*/
+
+class Solution2
+{
+public:
+    int climbStairs(int n)
+    {
+        vector<int> step(n + 1, 0);
+        if (n <= 0 || n == 1)
+            return 1;
+
+        //边界（起始）
+        step[0] = 1;
+        step[1] = 1;
+        for (int i = 2; i < n + 1; ++i)
+            step[i] = step[i - 1] + step[i - 2];
+
+        return step[n];
+    }
+};
+
+#ifdef USE_GTEST
+TEST(DSA, 70_climbStairs)
+{
+    Solution2 s;
+    int res1 = s.climbStairs(44);
+    ASSERT_TRUE(res1 == 1134903170);
+    int res2 = s.climbStairs(1);
+    ASSERT_TRUE(res2 == 1);
+}
+#endif
